c355c4966d5d376e835dbec29d7cb0c9b62cbac2
Antje Enzi <antje.enzi@gmail.com>
2019-09-05 10:12:20 +0200

    Update match method to filter bad img urls

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//this part will get the html from a given url and return it as a string, saved to the variable data
+//this part will get the html from a given url and return it as a string, saved to the variable "data"
 var https = require('https');
 let data;
 var options = {
@@ -20,8 +20,8 @@ request.on('error', function(e) {
 request.end();
 
 function main(data) {
-  matches = data.match(/"\/.+watermark=none" style/g); //call the string method "match" on the data variable
-  firstTen = matches.slice(0, 10); //slice result to the first first 10 urls
+  matches = data.match(/"\/[^\n#]+watermark=none" style/g); //call the string method "match" on the data variable
+  firstTen = matches.slice(0, 10); //slice result to the first 10 urls
   firstTenCropped = firstTen.map(url => url.slice(1, -7)); //apply the slice method to every element of the firstTen array, i.e. use a map to slice of the unnecessary "style"-tag at the end of each url
   firstTenFullUrl = firstTenCropped.map(
     https => (https = `https://memegen.link${https}`) //add the protocoll to the sliced urls
